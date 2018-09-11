dccf678efe9f1ed910a377bad8cbe73331b6bdd2
Eugene Christo <marteau9292@gmail.com>
2018-09-11 12:19:15 +0300

    Tough but finished

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  if (str.length < 1 || str.length % 2) return false;
+
+  for (let i = str.length - 1; i >= 0; i--) {
+    if (!str[i]) continue;
+    for (let j = 0; j < bracketsConfig.length; j++) {
+      if (str[i] === bracketsConfig[j][1]) {
+        str = str.replace(bracketsConfig[j].join(''), '');
+      }
+    }
+  }
+
+  return str ? false : true;
+};
